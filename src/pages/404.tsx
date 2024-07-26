c2cc6f3e4082ac95a10e399b575a25288bc7289f
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-26 09:54:03 +0100

    .

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -38,7 +38,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
           </>
         ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <Link to="/index">Go home</Link>.
       </p>
     </main>
   )
