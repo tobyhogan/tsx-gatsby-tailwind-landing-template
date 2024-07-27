77d0f65ef350dcb60c62529d7ae493890399e6cd
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-27 18:06:01 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import '../styles/index.css'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
+// @ts-ignore
 import tablescreenshot from '../assets/project-image.png'
 
 
