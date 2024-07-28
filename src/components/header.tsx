a692e573b614bc9a4c211b1f3e105e8c294d97fb
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-28 16:43:54 +0100

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,21 +12,7 @@ function Header() {
 
   const [navOpen, setNavOpen] = useState(false)
 
-  const [isDark, setIsDark] = React.useState(null)
-
-
-  
-  
-  useEffect(() => {
-
-    localStorage.setItem("test", "yes")
-
-    console.log(localStorage)
-
-
-
-  }, [])
-
+  const [isDark, setIsDark] = useState(false)
 
   const navList = 
     <>
@@ -37,24 +23,83 @@ function Header() {
     </>
 
 
-  
-
 
   useEffect(() => {
-    
-    if (isDark) {
+
+    console.log("2 effect running")
+
+    if (!localStorage.getItem("theme")) {
+
+
+      localStorage.setItem("theme", "light")
+      document.body.classList.add('light')
+
+      setIsDark(false)
+
+
+    } else if (localStorage.getItem("theme") == "light") {
+
+      
+      
+      document.body.classList.add('light');
+
+      setIsDark(false)
+
+
+
+    } else if (localStorage.getItem("theme") == "dark") {
+
+      
+      
+      document.body.classList.add('dark');
+
+      setIsDark(true)
+
+    }
+
+
+  }, [])
+
+
+  function handleToggleTheme() {
+
+    console.log("1 works")
+
+    if (localStorage.getItem("theme") == "light") {
+
+      console.log("3 change to dark")
+
+      localStorage.setItem("theme", "dark")
+
+      
       document.body.classList.add('dark');
       document.body.classList.remove('light');
 
+      setIsDark(true)
+      
 
-    } else if (isDark == false) {
-      document.body.classList.remove('dark');
+    } else if (localStorage.getItem("theme") == "dark") {
+
+      localStorage.setItem("theme", "light")
+
       document.body.classList.add('light');
-    }  
+      document.body.classList.remove('dark');
+
+      setIsDark(false)
+
+    } 
 
 
-  }, [isDark])
+  }
 
+
+  
+  
+
+
+
+
+  
 
 
 
@@ -96,8 +141,8 @@ function Header() {
 
         <div className="ThemeToggler">
 
-          { isDark ? (<button onClick={() => setIsDark(!isDark)}><MdOutlineDarkMode size={28} className="text-black dark:text-white"/></button>)
-            : (<button onClick={() => setIsDark(!isDark)}><MdLightMode size={28} className="text-black dark:text-white"/></button>) }
+          { isDark ? (<button onClick={handleToggleTheme}><MdOutlineDarkMode size={28} className="text-black dark:text-white"/></button>)
+            : (<button onClick={handleToggleTheme}><MdLightMode size={28} className="text-black dark:text-white"/></button>) }
 
         </div>
 
