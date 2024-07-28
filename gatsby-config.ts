3e52909896c34e3f3acaa3e07ea63a3c518bf3d4
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-28 16:54:21 +0100

    .

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        "icon": "src/assets/icon.svg"
+        "icon": "src/assets/icon.png"
       }
     },
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <div className="flex flex-col">
       <Header />
-      <main className="pb-20 h-[81.9vh]">
+      <main className="">
         <img src={tablescreenshot} className="border-2 border-slate-300 dark:border-slate-600 mt-12 rounded-lg"></img>
         <h1 className="text-center mt-8 text-2xl">Headline Goes Here</h1>
         <div className="flex flex-col mx-auto w-fit mt-8">
@@ -33,7 +33,7 @@ const IndexPage: React.FC<PageProps> = () => {
           <a href="https://habit-tracker-indol-ten.vercel.app/" className="mt-4 underline text-blue-400 mx-auto">Alternate Action</a>
         </div>
         <h1 className="text-center text-xl mt-8">Features:</h1>
-        <ul className="mx-auto w-fit mt-3">
+        <ul className="mx-auto w-fit mt-3 mb-[110px]">
           <li>- Feature 1</li>
           <li>- Feature 2</li>
           <li>- Feature 3</li>
